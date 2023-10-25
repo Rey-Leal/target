aa25a55be7861f8834c869174a7649be242bf084
ReyLeal <reinaldoleal2@gmail.com>
2023-10-25 09:08:28 -0300

    Layout

--- menu.php
+++ menu.php
@@ -1,21 +1,25 @@
-    <h5 class="light-blue-text text-darken-2">Testes Target</h5>
-    <a class="transparent light-blue-text text-darken-2" id="home" href="index.php">
-        <i class="material-icons">home</i>
-    </a>
-    <a class="transparent light-blue-text text-darken-2" id="1" href="exercicio1.php">
-        1<i class="material-icons">edit</i>
-    </a>
-    <a class="transparent light-blue-text text-darken-2" id="2" href="exercicio2.php">
-        2<i class="material-icons">edit</i>
-    </a>
-    <a class="transparent light-blue-text text-darken-2" id="3" href="exercicio3.php">
-        3<i class="material-icons">edit</i>
-    </a>
-    <a class="transparent light-blue-text text-darken-2" id="4" href="exercicio4.php">
-        4<i class="material-icons">edit</i>
-    </a>
-    <a class="transparent light-blue-text text-darken-2" id="5" href="exercicio5.php">
-        5<i class="material-icons">edit</i>
-    </a>
-    <br>
-    <br>
+<nav>
+    <div class="nav-wrapper light-blue darken-2 white-text">
+        <p class="brand-logo">Target</p>
+    </div>
+</nav>
+<a class="transparent light-blue-text text-darken-2" id="home" href="index.php">
+    <i class="material-icons">home</i>
+</a>
+<a class="transparent light-blue-text text-darken-2" id="1" href="exercicio1.php">
+    1<i class="material-icons">edit</i>
+</a>
+<a class="transparent light-blue-text text-darken-2" id="2" href="exercicio2.php">
+    2<i class="material-icons">edit</i>
+</a>
+<a class="transparent light-blue-text text-darken-2" id="3" href="exercicio3.php">
+    3<i class="material-icons">edit</i>
+</a>
+<a class="transparent light-blue-text text-darken-2" id="4" href="exercicio4.php">
+    4<i class="material-icons">edit</i>
+</a>
+<a class="transparent light-blue-text text-darken-2" id="5" href="exercicio5.php">
+    5<i class="material-icons">edit</i>
+</a>
+<br>
+<br>
